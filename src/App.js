f8c060502587ce72d0c8ad1182b6fb4f53a3e2d5
mohammad.seyedsaidi <mirnumbing@gmail.com>
2018-05-23 01:16:02 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import Search from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
 
-const key = 'AIzaSyDu3ULXjhGO0V-cEk5nmwrZWw1VrHp2IlA';
+const API_KEY = 'AIzaSyDu3ULXjhGO0V-cEk5nmwrZWw1VrHp2IlA';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            videos: []
+        }
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+
     render() {
         return (
             <div className="App">
