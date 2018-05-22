97c2a0b45e1ffc548ccfc7d5bb32153a49351782
mohammad.seyedsaidi <mirnumbing@gmail.com>
2018-05-23 00:59:39 +0200

    working on searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-class SearchBar extends React.Component{
-    render(){
-        return(
-            <input/>
+class SearchBar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            term: ''
+        }
+    }
+
+
+    onInputChange = event => {
+        this.setState({a: event.target.value})
+    }
+
+    render() {
+        return (
+            <div>
+                <input value={this.state.term} onChange={this.onInputChange}/>
+            </div>
         )
     }
 }
