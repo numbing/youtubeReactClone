46f336af79381c5c5219dbb9059c127adff8fba2
mohammad.seyedsaidi <mirnumbing@gmail.com>
2018-05-23 02:25:08 +0200

    video fetch to the dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Search from './components/search_bar'
 import YTSearch from 'youtube-api-search'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 
 const API_KEY = 'AIzaSyDu3ULXjhGO0V-cEk5nmwrZWw1VrHp2IlA';
@@ -10,18 +11,24 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            videos: []
-        }
+            videos: [],
+            selectedVideo : null
+
+        };
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({videos});
+            this.setState({
+                videos :videos,
+                selectedVideo : videos[0]
+            });
         });
     }
 
     render() {
         return (
             <div className="App">
-                <h1>hello</h1>
+
                 <Search/>
+                <VideoDetail video ={this.state.selectedVideo}/>
                 <VideoList videos={this.state.videos}/>
             </div>
         );
